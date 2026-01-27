26271d461886516ee2d5c311dfec79c050387fc0
thien-dev <dinhngocthien2004@gmail.com>
2026-01-27 20:51:04 +0700

    Start and StartSpawner

---
+++ Assets/Scripts/StarSpawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class StarSpawner : MonoBehaviour
+{
+    public GameObject starPrefab;
+    public float spawnAreaWidth = 8f;
+    public float spawnHeight = 6f;
+
+    void Start()
+    {
+        // Bắt đầu spawn ngẫu nhiên
+        StartCoroutine(SpawnStarsRandomly());
+    }
+
+    private System.Collections.IEnumerator SpawnStarsRandomly()
+    {
+        while (true)
+        {
+            float delay = Random.Range(0.5f, 3f); // thời gian chờ ngẫu nhiên
+            yield return new WaitForSeconds(delay);
+
+            // vị trí ngẫu nhiên theo trục X
+            float randomX = Random.Range(-spawnAreaWidth / 2, spawnAreaWidth / 2);
+            Vector3 spawnPos = new Vector3(randomX, spawnHeight, 0);
+
+            Instantiate(starPrefab, spawnPos, Quaternion.identity);
+        }
+    }
+}
