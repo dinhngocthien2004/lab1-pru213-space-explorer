f098cd8d9a556900cfeced4cbc7b42d9b5801128
thien-dev <dinhngocthien2004@gmail.com>
2026-01-27 20:39:07 +0700

    AutoShooter

---
+++ Assets/Scripts/AutoShooter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AutoShooter : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject bulletPrefab; // Kéo prefab đạn vào
+    public Transform firePoint;     // Vị trí bắn ra
+    public float fireRate = 0.5f;   // Tốc độ bắn (0.5 giây/bullet)
+
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= fireRate)
+        {
+            Shoot();
+            timer = 0;
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+    }
+}
