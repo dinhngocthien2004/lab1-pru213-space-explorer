46509cd75fbfee482ac9ef518b4ace4fafeea8bd
thien-dev <dinhngocthien2004@gmail.com>
2026-01-29 09:08:29 +0700

    Add PowerUp and ProjectileController

---
+++ Assets/Scripts/ProjectileController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class ProjectileController : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 10f;
+    [SerializeField] private Vector2 direction = Vector2.up;
+
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.linearVelocity = direction.normalized * moveSpeed;
+        handleProjectile();
+    }
+
+    public void handleProjectile()
+    {
+        Destroy(gameObject, 2f);
+    }
+
+    [SerializeField] private float damage = 1f; 
+    public float GetDamage() { return damage; }
+}
